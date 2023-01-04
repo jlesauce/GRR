4006f7181ee388b3844b469904065bb59e864842
Julien Le Sauce <julien.lesauce@gmail.com>
2023-01-04 17:04:24 +0100

    Add files ignored by .gitignore

---
+++ include/connect.inc.php
@@ -0,0 +1,16 @@
+<?php
+# Les quatre lignes suivantes sont à modifier selon votre configuration
+# ligne suivante : le nom du serveur qui herberge votre base sql.
+# Si c'est le même que celui qui heberge les scripts, mettre "localhost"
+$dbHost="dummy";
+# ligne suivante : le nom de votre base sql
+$dbDb="dummy";
+# ligne suivante : le nom de l'utilisateur sql qui a les droits sur la base
+$dbUser="dummy";
+# ligne suivante : le mot de passe de l'utilisateur sql ci-dessus
+$dbPass="dummy";
+# ligne suivante : Port MySQL laissé par défaut
+#$dbPort="3306";
+
+$table_prefix="resa";
+?>
